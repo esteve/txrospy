5d0fd8aef57b1b29f57952c216466450bfab7bd8
Esteve Fernandez <esteve@apache.org>
2013-05-17 14:27:15 +0000

    Added support for catkin

--- setup.py
+++ setup.py
@@ -1,20 +1,10 @@
-setupdict= {
-    'name': 'txROSpy',
-    'version': '0.1',
-    'author': 'Esteve Fernandez',
-    'author_email': 'esteve@apache.org',
-    'url': 'http://github.com/esteve/txrospy',
-    'description': 'Twisted port of the ROS protocols',
-    'long_description': '''txROSpy provides implementations of the ROS protocols (publisher, subscriber, service and client) suitable for their use in Twisted asynchronous applications.'''
-    }
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
-    setupdict['py_modules'] = ['txrospy']
-else:
-    setupdict['py_modules'] = ['txrospy']
-    setupdict['install_requires'] = ['Twisted']
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    package_dir={'': 'src'},
+    packages=['']
+)
 
-setup(**setupdict)
+setup(**setup_args)
